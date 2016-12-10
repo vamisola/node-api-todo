1daa2c1c96226b0e632ebff5a1ac87c742ebdd88
Vina Compson <vv.compson@icloud.com>
2016-12-10 06:55:50 +0000

    modify mongodb-delete.js deleting bella

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,34 +1,38 @@
 const MongoClient = require('mongodb').MongoClient;
 // const {MongoClient, ObjectID} = require('mongodb');
 
+//mongoose.Promise = global.Promise;
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
 
-  // deleteMany
-  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
-  //   console.log(result);
-  // });
+//deleteMany
+// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+//   console.log(result);
+// });
 
-  // deleteOne
-  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
-  //   console.log(result);
-  // });
+// db.collection('users').deleteMany({name: 'Bella'}).then((result) => {
+//   console.log(result);
+// });
 
-  // findOneAndDelete
-  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
-  //   console.log(result);
-  // });
+//deleteOne
+// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+//   console.log(result);
+// });
 
-  // db.collection('Users').deleteMany({name: 'Andrew'});
+//findOneAnd Delete
+// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+//   console.log(result);
+// });
 
-  // db.collection('Users').findOneAndDelete({
-  //   _id:ObjectId("584b9373c1b554822b37c8fa")
-  // }).then((results) => {
-  //   console.log(JSON.stringify(results, undefined, 2));
-  // });
-
+// db.collection('users').findOneAndDelete({
+//   "_id": ObjectID("584b9373c1b554822b37c8fa")
+// }).then((result) => {
+//   console.log(JSON.stringify(result, undefined, 2));
+// });
+  
   // db.close();
 });
