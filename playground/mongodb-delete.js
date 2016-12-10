70376e484ef761b44c71f54f391740370451e83d
Vina Compson <vv.compson@icloud.com>
2016-12-10 06:48:44 +0000

    modify mongo-db-delete

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,21 +1,34 @@
 const MongoClient = require('mongodb').MongoClient;
 // const {MongoClient, ObjectID} = require('mongodb');
 
-//mongoose.Promise = global.Promise;
-
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
 
-//deleteMany
-db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
-  console.log(result);
-});
-//deleteOne
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
 
-//findOneAnd Delete
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Users').deleteMany({name: 'Andrew'});
+
+  // db.collection('Users').findOneAndDelete({
+  //   _id:ObjectId("584b9373c1b554822b37c8fa")
+  // }).then((results) => {
+  //   console.log(JSON.stringify(results, undefined, 2));
+  // });
 
   // db.close();
 });
