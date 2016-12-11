1718783f00d0634ba69d650997ad5130473db014
Vina Compson <vv.compson@icloud.com>
2016-12-11 12:03:50 +0000

    add server folder

---
+++ server/server.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongoose = require('./db/mongoose');
+var Todo = require('./models/todo');
+var User = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, () => {
+    console.log('server is up and running!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var otherTodo = new Todo({
+//     text: 'Feed the dog',
+//     completed: true,
+//     completedAt: 123
+// });
+
+// otherTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('Unable to save', e);
+// });
+
+//User
+//email - require - trim it - set type - set min lenght of 1
+
+
+
+
+// var newUser = new User({
+//     email: 'test!123.com',
+// });
+
+// newUser.save().then((doc) => {
+//     console.log('User saved', doc);
+// }, (e) => {
+//     console.log('Unable to save user', e);
+// });
