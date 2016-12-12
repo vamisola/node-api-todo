4a707d16ae74e37e2674c9bf58e40e68b8e0036a
Vina Compson <vv.compson@icloud.com>
2016-12-12 00:26:57 +0000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log('server is up and running!');
 });
